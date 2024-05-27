5de92549ac7d13a5e792008be8bc9451dfd24feb
Tylar Pinniger <tylarpinniger@hotmail.com>
2024-05-27 15:21:55 +1000

    added print statements

--- AT2.py
+++ AT2.py
@@ -5,53 +5,53 @@ import socket
 import speedtest
 import datetime
 
-# dictionary 
+# Dictionary 
 info = {} 
 
-# computer name
+# Computer name
 info["Computer Name"] = socket.gethostname()
 
-# ip address
+# IP address
 info["IP Address"] = socket.gethostbyname(socket.gethostname())
 
-# mac address
+# MAC address
 info["MAC Address"] = hex(uuid.getnode())
 
-# processor name 
-processor_name = platform.processor() 
+# Processor name 
+processorName = platform.processor() 
 
-# adding it to dictionary 
-info["Processor Model"] = processor_name 
+# Adding processor name to dictionary. 
+info["Processor Model"] = processorName 
 
-# platform details 
-platform_details = platform.platform() 
+# Platform details 
+platformDetails = platform.platform() 
 
-# adding it to dictionary 
-info["Operation System"] = platform_details 
+# Adding platform details to dictionary. 
+info["Operation System"] = platformDetails 
 
-# ct stores current time
+# ct stores current time.
 ct = datetime.datetime.now()
 info["System Time"] =  ct
 
 def perform_speed_test():
    st = speedtest.Speedtest()
-   download_speed = st.download() / 1000000  # Convert to Mbps
-   return round(download_speed, 2)
+   downloadSpeed = st.download() / 1000000  # Convert to Mbps
+   return round(downloadSpeed, 2)
 
-download_speed = perform_speed_test()
-info["Internet Connection Speed"] = str(download_speed) + " Mbps"
+downloadSpeed = perform_speed_test()
+info["Internet Connection Speed"] = str(downloadSpeed) + " Mbps"
 
 
-#Python code for simple port scanning
-ip = socket.gethostbyname (socket.gethostname()) #getting ip-address of host
+# Simple port scanning
+ip = socket.gethostbyname (socket.gethostname()) # Getting ip-address of host
 
 ports = ""
 
-for port in range(65535):	 #check for all available ports
+for port in range(65535):	 # Check for all available ports
 
 	try:
-		serv = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # create a new socket
-		serv.bind((ip,port)) # bind socket with address
+		serv = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # Create a new socket
+		serv.bind((ip,port)) # Bind socket with address
 			
 	except:
 		if ports == "":
@@ -59,31 +59,38 @@ for port in range(65535):	 #check for all available ports
 		else:
 			ports = ports + ";" + str(port)
 		
-	serv.close() #close connection
+	serv.close() # Close connection
 
+# Adding active ports to dictionary.
 info["Active Ports"] =  ports
 
-# printing the details 
+# Printing the dictionary details 
 for i, j in info.items(): 
 	print(i, " - ", j) 
 	
-file = "computers.csv"
+file = "C:/Users/tylarp/Documents/VS Code/computers.csv"
 
-with open(file, newline='') as csvfile:
+# Reading csv file and checking the MAC address.
+with open(file, newline="") as csvfile:
     reader = csv.DictReader(csvfile)
     macExists = False
     for row in reader:
-        mac_address = row['MAC Address']
-        print(mac_address + " has been read")
-		
-        if mac_address == info["MAC Address"]:
+        macAddress = row["MAC Address"]
+        print(macAddress + " has been read")
+		# MAC address is already in file
+        if macAddress == info["MAC Address"]:
             print("computer already added to file")
             macExists = True
 
 csvfile.close()
 
+# Writing information to csv file if MAC address does not exist.
 if macExists == False:
-    csvfile = open(file, 'a')
-    writer = csv.DictWriter(csvfile, fieldnames=list(info.keys()))
-    writer.writerow(info)
-    csvfile.close()
+    try:
+        csvfile = open(file, "a")
+        writer = csv.DictWriter(csvfile, fieldnames=list(info.keys()))
+        writer.writerow(info)
+        csvfile.close()
+        print("computer added to csv file")
+    except:
+        print("error writing to csv file")
